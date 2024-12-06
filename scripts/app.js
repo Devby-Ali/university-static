47ae9280aa146d00e8c8f3c17c4754fa277587ca
Ali Bakhshi <alibakhshi962002@gmail.com>
2024-12-06 20:36:44 +0330

    styling footer

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,30 @@
-const navBtn = document.querySelector(".nav__btn")
-const navMenu = document.querySelector(".nav-menu")
+// const navBtn = document.querySelector(".nav__btn")
+// const navMenu = document.querySelector(".nav-menu")
+const loginButton = document.getElementById("login-button");
+const nameInput = document.getElementById("name");
+const passInput = document.getElementById("pass");
 
-let navOpen = false;
-navBtn.addEventListener("click", function () {
-    if (navOpen) {
-        navBtn.classList.remove("nav__btn--open")
-        navMenu.classList.remove("nav-menu--open")
+// let navOpen = false;
+// navBtn.addEventListener("click", function () {
+//     if (navOpen) {
+//         navBtn.classList.remove("nav__btn--open")
+//         navMenu.classList.remove("nav-menu--open")
 
-        navOpen = false
+//         navOpen = false
+//     } else {
+//         navBtn.classList.add("nav__btn--open")
+//         navMenu.classList.add("nav-menu--open")
+//         navOpen = true
+//     }
+// })]
+
+
+
+loginButton.addEventListener("click", () => {
+    if (nameInput.value.length >= 3 && passInput.value.length >= 8) {
+        alert("ورود موفقیت آمیز")
+        location.href = "/";
     } else {
-        navBtn.classList.add("nav__btn--open")
-        navMenu.classList.add("nav-menu--open")
-        navOpen = true
+        alert("نام کاربری حداقل 3 کاراکتر و رمز حداقل 8 کاراکتر")
     }
-})
+});
